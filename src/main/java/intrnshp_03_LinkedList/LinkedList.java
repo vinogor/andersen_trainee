b02e9ee471575b44862e476f8c1d3ee51d77bd59
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2019-03-13 14:36:19 +0300

    - class ItemOfLinkedList become inner class of LinkedList class - in LinkedList class create some methods: -- add (int the end / by index) -- del (last one / by index / by object)

--- src/main/java/intrnshp_03_LinkedList/LinkedList.java
+++ src/main/java/intrnshp_03_LinkedList/LinkedList.java
@@ -6,8 +6,8 @@ public class LinkedList<T> {
     private ItemOfLinkedList<T> last;
     private int size = 0;
 
+    // добавить в конец
     public void add(T obj) {
-
         if (isEmpty()) {
             ItemOfLinkedList<T> newItem = new ItemOfLinkedList<>(null, null, obj);
             first = last = newItem;
@@ -18,9 +18,42 @@ public class LinkedList<T> {
         size++;
     }
 
-    // Добавление объекта в середину связанного списка
-    public void add(int index, T obj) {
+    // Добавление объекта в по индексу
+    public void add(int index, T obj) throws IndexOutOfBoundsException {
+        if ((index > size) || (index < 0)) {
+            throw new IndexOutOfBoundsException("ERROR: index is not correct");
+        }
+        if (index == size) {
+            add(obj);
+        } else {
+            // делаем вставку
+            ItemOfLinkedList<T> shiftedRightItem = findItemByIndexUnsafe(index);
+            ItemOfLinkedList<T> newItem = new ItemOfLinkedList<>(shiftedRightItem.prevItem, shiftedRightItem, obj);
+            if (index == 0) { // если надо вставить в самое начало
+                first = newItem;
+            } else {
+                shiftedRightItem.prevItem.nextItem = newItem;
+            }
+            shiftedRightItem.prevItem = newItem;
+            size++;
+        }
+    }
 
+    // найти элемент по индексу (без проверки на существование index)
+    private ItemOfLinkedList<T> findItemByIndexUnsafe(int index) {
+        ItemOfLinkedList<T> desiredItem;
+        if (index < size / 2) {
+            desiredItem = first;
+            for (int i = 0; i < index; i++) {
+                desiredItem = desiredItem.nextItem;
+            }
+        } else {
+            desiredItem = last;
+            for (int i = 0; i < size - index - 1; i++) {
+                desiredItem = desiredItem.prevItem;
+            }
+        }
+        return desiredItem;
     }
 
     // Удаление последнего элемента
@@ -34,8 +67,57 @@ public class LinkedList<T> {
     }
 
     // Удаление элемента по индексу
-    // Удаление элемента по значению объекта
+    public void del(int index) {
+        if ((index > size - 1) || (index < 0)) {
+            throw new IndexOutOfBoundsException("ERROR: index is not correct");
+        }
+        if (index == size - 1) {
+            last.prevItem.nextItem = null;
+            last = last.prevItem;
+        } else {
+            ItemOfLinkedList<T> deletedItem = findItemByIndexUnsafe(index);
+            if (index == 0) { // если надо удалить из самого начала
+                first = deletedItem.nextItem;
+                deletedItem.nextItem.prevItem = null;
+            } else {
+                deletedItem.prevItem.nextItem = deletedItem.nextItem;
+                deletedItem.nextItem.prevItem = deletedItem.prevItem;
+            }
+        }
+        size--;
+    }
 
+    // Удаление элемента по значению объекта
+    public void del(T obj) throws IndexOutOfBoundsException {
+        ItemOfLinkedList<T> deletedItem = findItemByObj(obj);
+        if (deletedItem == null) {
+            throw new IndexOutOfBoundsException("ERROR: no such object found");
+        }
+        if (deletedItem.nextItem == null) {
+            deletedItem.prevItem.nextItem = null;
+            last = deletedItem.prevItem;
+        } else if(deletedItem.prevItem == null) {
+            deletedItem.nextItem.prevItem = null;
+            first = deletedItem.nextItem;
+        } else {
+            deletedItem.prevItem.nextItem = deletedItem.nextItem;
+            deletedItem.nextItem.prevItem = deletedItem.prevItem;
+        }
+    }
+
+    private ItemOfLinkedList<T> findItemByObj(T obj) {
+        ItemOfLinkedList<T> item = first;
+        do {
+            if ((item.obj == null) && (obj == null)) {
+                return item;
+            }
+            if ((item.obj != null) && (item.obj.equals(obj))) {
+                return item;
+            }
+            item = item.nextItem;
+        } while (item != null);
+        return null;
+    }
 
     public void clear() throws IndexOutOfBoundsException {
         if (isEmpty()) {
@@ -47,15 +129,15 @@ public class LinkedList<T> {
 
     public void print() {
         ItemOfLinkedList<T> itemForPrint = first;
-
         if (isEmpty()) {
             System.out.println("LinkedList is empty");
         } else {
             do {
-                System.out.println(itemForPrint + " | " + itemForPrint.prevItem + " | " + itemForPrint.obj + " | " + itemForPrint.nextItem);
+                System.out.print(itemForPrint.obj + " | ");
                 itemForPrint = itemForPrint.nextItem;
             } while (itemForPrint != null);
         }
+        System.out.println();
     }
 
     // Получение объекта последнего элемента
@@ -72,5 +154,17 @@ public class LinkedList<T> {
 
     private boolean isEmpty() {
         return size == 0;
+    }
+
+    private static class ItemOfLinkedList<T> {
+        ItemOfLinkedList<T> prevItem;
+        ItemOfLinkedList<T> nextItem;
+        T obj;
+
+        public ItemOfLinkedList(ItemOfLinkedList<T> prevItem, ItemOfLinkedList<T> nextItem, T obj) {
+            this.prevItem = prevItem;
+            this.nextItem = nextItem;
+            this.obj = obj;
+        }
     }
 }
